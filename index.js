a308176fa112a8c7ba7cf48773a6c57fa001ea86
Tammarra McPhaul <shaemc22@gmail.com>
2022-01-27 19:48:36 -0500

    commit tree e309d763d97bb84f142f1db487d113f19b4243ac

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string) {
+    let arr = string.split(" ");
+    let result = [];
+    let noWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    for(let i = 0; i < arr.length; i++){
+      if (noWords.includes(arr[i]) === false || (i === 0)){
+        result.push(this.capitalize(arr[i]));
+      } else {
+        result.push(arr[i]);
+      }
+    }
+
+    let str = result.join(" ");
+    return str;
+  }
 }
